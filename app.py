bef42243d11c3f6399b9ceeccb6df4ba209b2fa9
Bobby <bobbybabu@gmail.com>
2023-10-28 13:34:37 -0400

    this is my second commit

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Bobby"
-
+    return "Learning DevOps with Git"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
