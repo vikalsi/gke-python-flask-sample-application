d46927d4d7168b76e492ab253f9b1977f7cedb2b
vikalsi <kalsi.inder001@gmail.com>
2023-11-01 20:58:44 -0400

    changed print out message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Learning DevOps with Git"
+    return "Learning Devops with Pragra"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
